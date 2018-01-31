829014556c8d02ea29552e84de70d99744cbfcbe
Ray Marceau <razorman8669@users.noreply.github.com>
2018-01-30 16:40:32 -0800

    run as sudoer instead of root

--- upgrade.sh
+++ upgrade.sh
@@ -4,9 +4,9 @@
 # Add the following to the crontab (i.e. crontab -e)
 # */120 * * * * ~/smartnode/upgrade.sh
 
-if apt list --upgradable | grep -v grep | grep smartcashd > /dev/null
+if sudo apt list --upgradable | grep -v grep | grep smartcashd > /dev/null
 then
-  smartcash-cli stop && sleep 20 && apt update && apt install smartcashd -y && sleep 20 && smartcashd
+  smartcash-cli stop && sleep 20 && sudo apt update && sudo apt install smartcashd -y && sleep 20 && smartcashd
 else
   exit
 fi
